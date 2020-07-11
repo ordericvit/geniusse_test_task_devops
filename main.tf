47152cc55580508be5989f96a3b75611a0b0e9cb
orderic <orderic.vit@gmail.com>
2020-07-11 19:30:36 +0300

    test_task_v2

--- main.tf
+++ main.tf
@@ -72,7 +72,6 @@ data "google_client_config" "default" {
 provider "kubernetes" {
   load_config_file = false
     host = "${google_container_cluster.primary.endpoint}"
-    #host = data.google_container_cluster.primary.endpoint
     token = "${data.google_client_config.default.access_token}"
     cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
 }
